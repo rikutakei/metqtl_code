6da9d067e32c31df8705384b6b4cc72c38ec84d8
rikutakei <rikut777@gmail.com>
2024-05-03 13:39:37 -0500

    Fixed header for making MR sumstats

--- src/mr/select_mr_variants.sh
+++ src/mr/select_mr_variants.sh
@@ -10,5 +10,5 @@ module load parallel
 # Make a list of all the variants and pull these out from the summary stats:
 cat /scratch/rtakei/projects/2023/metqtl_project/data/mr_dat/*mr_snps.txt | sort | uniq > /scratch/rtakei/projects/2023/metqtl_project/data/mr_dat/all_mr_snps.txt
 
-parallel "grep -Fwf /scratch/rtakei/projects/2023/metqtl_project/data/mr_dat/all_mr_snps.txt {} > {.}.mr.txt " ::: $(ls /scratch/rtakei/projects/2023/metqtl_project/data/mr_dat/*sumstats.txt)
+parallel "cat <(head -1 {}) <(grep -Fwf /scratch/rtakei/projects/2023/metqtl_project/data/mr_dat/all_mr_snps.txt {}) > {.}.mr.txt " ::: $(ls /scratch/rtakei/projects/2023/metqtl_project/data/mr_dat/*sumstats.txt)
 
